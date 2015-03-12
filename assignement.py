6d86bdd2c99ef21ada1327e3b63553ee12a0e3cd
plagree <paul.lagree@gmail.com>
2015-03-12 22:25:40 +0100

    New orders for assignement

--- assignement.py
+++ assignement.py
@@ -1,3 +1,4 @@
+from random import shuffle
 
 def server_has_space(server, DC, line, column, S):
     if server.size+column > S:
@@ -11,15 +12,19 @@ def put_servers(DC, servers, nb_lines, S):
     sortedServers = sorted(servers.values(), key=lambda x: [x.capacity, x.size], reverse=True)
     line = 0
     column = 0
+    lines = range(nb_lines)
+    #shuffle(lines)
     for server in sortedServers:
         currLine = line
         while(True):
-            if server_has_space(server, DC, line, column, S):
-                server.line = line
+            if server_has_space(server, DC, lines[line], column, S):
+                server.line = lines[line]
                 server.column = column
                 for j in range(column, column+server.size):
-                    DC[line][j] = server.index
+                    DC[lines[line]][j] = server.index
                 line += 1
+                if line == nb_lines:
+                    pass #lines = sorted(lines, key=lambda l: DC[l].count(None))
                 line %= nb_lines
                 break
             column += 1
@@ -27,6 +32,8 @@ def put_servers(DC, servers, nb_lines, S):
                 column = 0
                 line += 1
                 line %= nb_lines
+                if line == nb_lines:
+                    pass #lines = sorted(lines, key=lambda l: DC[l].count(None))
                 if line == currLine:
                     break
 
