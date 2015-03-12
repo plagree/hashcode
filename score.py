7a12da68e366488841327b836a21ae5399bcf983
Romain Pomier <romain.pomier@pricematch.travel>
2015-03-12 22:21:30 +0100

    Improve score function

--- score.py
+++ score.py
@@ -9,8 +9,13 @@ def compute_score(servers, nb_groups, nb_lines):
 
 def garantied_capacity(servers, nb_lines, group_id):
     total_capacity = sum([s.capacity if s.group == group_id else 0 for s in servers])
+    by_line = {l: [] for l in range(nb_lines)}
+    for s in servers:
+        if s.line is None or s.group != group_id:
+            continue
+        by_line[s.line].append(s)
     value = total_capacity
     for l in range(nb_lines):
-        capacity = sum([s.capacity if s.group == group_id and s.line != l else 0 for s in servers])
+        capacity = total_capacity - sum([s.capacity for s in by_line[l]])
         value = min(value, capacity)
     return value
